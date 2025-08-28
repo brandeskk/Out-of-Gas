7a59bb99b3d97c4c065db5b40c15572645844627
brandeskk <gabriela.brandes@alunos.sc.senac.br>
2025-08-28 14:01:07 -0300

    movimento

---
+++ Assets/Scripts/movimento.cs
@@ -0,0 +1,46 @@
+
+public class movimento : MonoBehaviour
+{
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MoveAnimationLogic(movement);
+
+    }
+
+    void MoveAnimationLogic(Vector2 moveSpeed)
+    {
+        if (moveSpeed.y < 0)
+        {
+            animator.Play("replace");
+            return;
+        }
+
+        if (moveSpeed.y > 0)
+        {
+            animator.Play("replace");
+            return;
+        }
+
+        if (moveSpeed.x < 0)
+        {
+            animator.Play("replace");
+            return;
+        }
+
+        if (moveSpeed.x > 0)
+        {
+            animator.Play("replace");
+            return;
+        }
+        animator.Play("replace");
+    }
+}
