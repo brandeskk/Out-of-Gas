63a997d11c54bf9f9f78f619721d3520ba10f42f
CalebeVaine <calebe.ferreira@alunos.sc.senac.br>
2025-08-29 11:35:00 -0300

    Salvar

--- Assets/Scripts/Interagir.cs
+++ Assets/Scripts/Interagir.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Interagir : MonoBehaviour
 {
-    public float interactRange = 2f; // Distância máxima para interação
-    public LayerMask interactableLayer; // Layer dos objetos interagíveis
+    public float interactRange = 2f;
+    public LayerMask interactableLayer; 
 
     void Update()
     {
@@ -15,22 +15,21 @@ public class Interagir : MonoBehaviour
 
     void Interact()
     {
-        // Detecta todos os objetos interagíveis dentro do raio
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, interactRange, interactableLayer);
 
         foreach (Collider2D hit in hits)
         {
-            // Procura a interface IInteractable no objeto
+           
             IInteractable interactable = hit.GetComponent<IInteractable>();
             if (interactable != null)
             {
-                interactable.Interact(); // Chama a interação
-                break; // Interage com apenas um objeto por vez
+                interactable.Interact(); 
+                break; 
             }
         }
     }
 
-    // Mostra a área de interação no editor
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
